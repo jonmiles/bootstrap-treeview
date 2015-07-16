7f16a27358b010874ad1f7c6f48d3501542a1ecf
aguyawry <selson@ElsonMBP.local>
2015-07-16 11:28:04 -0400

    forgot to build :(

--- public/js/bootstrap-treeview.js
+++ public/js/bootstrap-treeview.js
@@ -700,7 +700,7 @@
 		badge: '<span class="badge"></span>'
 	};
 
-	Tree.prototype.css = '.treeview .list-group-item{cursor:pointer}.treeview span.indent{margin-left:10px;margin-right:10px}.treeview span.icon{width:12px;margin-right:5px}.treeview .node-disabled{color:silver;cursor:not-allowed}'
+	Tree.prototype.css = '.treeview .list-group-item{cursor:pointer;overflow:hidden;text-overflow:ellipsis;white-space: nowrap;}.treeview span.indent{margin-left:10px;margin-right:10px}.treeview span.icon{width:12px;margin-right:5px}.treeview .node-disabled{color:silver;cursor:not-allowed}'
 
 
 	/**
