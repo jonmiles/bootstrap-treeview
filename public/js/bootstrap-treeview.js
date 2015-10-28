560c78a2028a0b56fb093159c121e996a9263dc7
Rein Aris <info@reinaris.nl>
2015-10-28 13:47:54 +0000

    Check if there are nodes before adding expandIcon

--- public/js/bootstrap-treeview.js
+++ public/js/bootstrap-treeview.js
@@ -527,7 +527,7 @@
 
 			// Add expand, collapse or empty spacer icons
 			var classList = [];
-			if (node.nodes) {
+			if (node.nodes && node.nodes.length > 0) {
 				classList.push('expand-icon');
 				if (node.state.expanded) {
 					classList.push(_this.options.collapseIcon);
