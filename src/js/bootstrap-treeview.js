046b388ca60b9076b9ed2123b203d0dff4a2ab8a
Josh Pendergrass <comeroj@hotmail.com>
2016-06-30 12:18:12 -0700

    fixed issue with re rendering the expand icon

--- src/js/bootstrap-treeview.js
+++ src/js/bootstrap-treeview.js
@@ -795,6 +795,8 @@
 		}
 		else {
 		    node.$el.find(".node-icon").remove();
+		    node.$el.find(".expand-icon").remove();
+            
 		    node.$el.find(".indent").remove()
 		    node.$el.find(".badge").remove()
 		    text = node.$el.find(".node-text");
