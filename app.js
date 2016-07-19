463ca39c624119ba325c22d69c204d9891305fd2
Dávid Halász <dhalasz@redhat.com>
2016-07-19 10:47:54 +0200

    Update ExpressJS to the latest version
    The old 3.x versions are no longer maintained.
    Also their dependencies can cause security issues.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@
  */
 
 var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorhandler = require('errorhandler');
 var http = require('http');
 var path = require('path');
 
@@ -11,17 +15,16 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(methodOverride());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/tests')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorhandler());
 }
 
 http.createServer(app).listen(app.get('port'), function(){
