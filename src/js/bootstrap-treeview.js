3b00da2bdcdb64e27e083c5d70f3e4ac1108a2bc
Dávid Halász <dhalasz@redhat.com>
2016-09-09 14:19:19 +0200

    Clone cached DOM elements instead of creating new ones from string

--- src/js/bootstrap-treeview.js
+++ src/js/bootstrap-treeview.js
@@ -741,7 +741,7 @@
 		if (!this._initialized) {
 
 			// Setup first time only components
-			this.$wrapper = $(this._template.tree);
+			this.$wrapper = this._template.tree.clone();
 			this.$element.empty()
 				.addClass(pluginName)
 				.append(this.$wrapper);
@@ -776,19 +776,19 @@
 
 		// Add indent/spacer to mimic tree structure
 		for (var i = 0; i < (node.level - 1); i++) {
-			node.$el.append(this._template.indent);
+			node.$el.append(this._template.indent.clone());
 		}
 
 		// Add expand / collapse or empty spacer icons
 		node.$el
-			.append($(this._template.icon)
+			.append(this._template.icon.clone()
 				.addClass(node.nodes ? 'expand-icon' : this._options.emptyIcon)
 			);
 
 		// Add node icon
 		if (this._options.showIcon) {
 			node.$el
-				.append($(this._template.icon)
+				.append(this._template.icon.clone()
 					.addClass('node-icon')
 					.addClass(node.icon || this._options.nodeIcon)
 				);
@@ -797,7 +797,7 @@
 		// Add checkable icon
 		if (this._options.showCheckbox) {
 			node.$el
-				.append($(this._template.icon)
+				.append(this._template.icon.clone()
 					.addClass('check-icon')
 				);
 		}
@@ -809,7 +809,7 @@
 		if (this._options.showTags && node.tags) {
 			$.each(node.tags, $.proxy(function addTag(id, tag) {
 				node.$el
-					.append($(this._template.badge)
+					.append(this._template.badge.clone()
 						.append(tag)
 					);
 			}, this));
@@ -830,7 +830,7 @@
 	// Creates a new node element from template and
 	// ensures the template is inserted at the correct position
 	Tree.prototype._newNodeEl = function (node, previousNode) {
-		var $el = $(this._template.node);
+		var $el = this._template.node.clone();
 
 		if (previousNode) {
 			// typical usage, as nodes are rendered in
@@ -951,11 +951,11 @@
 	};
 
 	Tree.prototype._template = {
-		tree: '<ul class="list-group"></ul>',
-		node: '<li class="list-group-item"></li>',
-		indent: '<span class="indent"></span>',
-		icon: '<span class="icon"></span>',
-		badge: '<span class="badge"></span>'
+		tree: $('<ul class="list-group"></ul>'),
+		node: $('<li class="list-group-item"></li>'),
+		indent: $('<span class="indent"></span>'),
+		icon: $('<span class="icon"></span>'),
+		badge: $('<span class="badge"></span>')
 	};
 
 	Tree.prototype._css = '.treeview .list-group-item{cursor:pointer}.treeview span.indent{margin-left:10px;margin-right:10px}.treeview span.icon{width:12px;margin-right:5px}.treeview .node-disabled{color:silver;cursor:not-allowed}'
