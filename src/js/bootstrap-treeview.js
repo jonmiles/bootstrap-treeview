aef99a0487753f4469942215f301d1ef54f72668
lsowen <logan@s1network.com>
2017-02-28 19:28:50 -0500

    Implement _sortOrder which maintains order for more than 10 nodes.
    Fix for https://github.com/jonmiles/bootstrap-treeview/issues/251.

--- src/js/bootstrap-treeview.js
+++ src/js/bootstrap-treeview.js
@@ -442,8 +442,27 @@
 	};
 
 	Tree.prototype._sortNodes = function () {
-		return $.map(Object.keys(this._nodes).sort(), $.proxy(function (value, index) {
-		  return this._nodes[value];
+		// based on http://stackoverflow.com/a/16187766/3108853
+		function logicalSort(a, b) {
+			var i, diff;
+			var regExStrip0 = /(\.0+)+$/;
+			var segmentsA = a.replace(regExStrip0, '').split('.');
+			var segmentsB = b.replace(regExStrip0, '').split('.');
+
+			var len = Math.min(segmentsA.length, segmentsB.length);
+
+			for(i = 0; i < length; i++){
+				diff = parseInt(segmentsA[i], 10) - parseInt(segmentsB[i], 10);
+				if(diff) {
+					return diff;
+				}
+			}
+
+			return segmentsA.length - segmentsB.length;
+		}
+
+		return $.map(Object.keys(this._nodes).sort(logicalSort), $.proxy(function (value, index) {
+			return this._nodes[value];
 		}, this));
 	};
 
